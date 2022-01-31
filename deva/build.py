ae3cad8f6e46224de4c00af682b8aa7795db064a
rphpacheco <rphpacheco@gmail.com>
2022-01-31 00:12:07 -0300

    build create file in folder

--- deva/build.py
+++ deva/build.py
@@ -2,16 +2,46 @@ import os
 from .tree import tree
 
 folders = [
-    'stage/extract',
-    'stage/transform',
-    'stage/load',
+    ['app/prd',[
+        'application.pbix'
+    ]],
+    ['app/qa',[
+        'application.pbix'
+    ]],
+    ['loads/extract', [
+        'extract.qvs',
+        'extract.qvw',
+    ]],
+    ['loads/transform', [
+        'transform.qvs',
+        'transform.qvw',
+    ]],
+    ['loads/model', [
+        'model.qvs',
+        'model.qvw',
+    ]],
+    ['config', [
+        'config.qvs'
+    ]],
 ]
 
 def build():
     print('build project structure')
-    path = os.getcwd()
+    rootPath = os.getcwd()
 
     for folder in folders:
-        os.makedirs(os.path.join(path, folder))
+        path = os.path.join(rootPath, folder[0])
+        if os.path.exists(path):
+            print(f'directory {folder[0]} has exists!')
+        else:
+            os.makedirs(path)
+        
+        for file in folder[1]:
+            fileName = os.path.join(path, file)
+            if os.path.exists(fileName):
+                print(f'file {file} has exists')
+            else:
+                with open(fileName, 'a'):
+                    os.utime(fileName, None)
     
-    tree(path)
+    tree(rootPath)
